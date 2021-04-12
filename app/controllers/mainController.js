373e515081a51d1569751b220523f7a53a6c21a4
Segolene-Danjou <segolene.danjou@gmail.com>
2021-04-12 19:19:33 +0200

    ajout du projet

---
+++ app/controllers/mainController.js
@@ -0,0 +1,31 @@
+const dataMapper = require('../dataMapper.js');
+
+const mainController = {
+  homePage: (req, res) => {
+    dataMapper.getAllCards( (err, cards) => {
+      if(err) {
+        console.error(err);
+        return;
+      } 
+      //console.log(cards);
+      res.render('cardList', {
+        cards: cards.rows,
+        title: 'Liste des cartes'
+      })
+    });
+  },
+
+  cardPage: (req, res) => {
+    dataMapper.getOneCard(req.params.id, (err, cards) => {
+      if(err) {
+        console.error(err);
+        return res.send(err);
+      } 
+
+      res.render('details', cards.rows[0]);
+
+    });
+  }
+};
+
+module.exports = mainController;
